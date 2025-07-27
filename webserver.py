a8a542a05b7809dbfb58c3cfb8ab94446d1b6fed
Alan <aantoalan@gmail.com>
2025-07-27 12:11:00 +0530

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,12 +1,12 @@
-from flask import Flask
-import threading
-app = Flask("")
-@app.route('/')
+from flask import Flask #flask web framework for running this file
+import threading 
+app = Flask("") #creating an instance of flask 
+@app.route('/') #this means , that the decorator in flask allows us to associate url with a function , or something idk
 def home():
-    return 'Bot is working!'
+    return 'Bot is working!'  #to let us know Bot is working 
 def run():
-    app.run(host='0.0.0.0',port=8080)
-def keep_alive():
+    app.run(host='0.0.0.0',port=8080) #the host being 0.9.0.0 allows us to enable all ip addresses to work 
+def keep_alive(): #called in main.py
     t=threading.Thread(target=run)
-    t.start()
+    t.start() #starting the thread , which runs the run func defined prev
 
